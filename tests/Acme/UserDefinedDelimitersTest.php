e4ad1a2fa0fa4a27862b0532cd7cb0dff73ee4dc
Kore Nordmann <mail@kore-nordmann.de>
2014-08-09 14:55:18 +0200

    Added step with user defined delimiters

---
+++ tests/Acme/UserDefinedDelimitersTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Acme;
+
+/**
+ * Support different delimiters to change a delimiter, the beginning of the
+ * string will contain a separate line that looks like this:
+ *
+ * "//[delimiter]\n[numbers"]" for example "//;\n1;2" should return three where
+ * the default delimiter is ";". The first line is optional.
+ *
+ * @group step4
+ */
+class UserDefinedDelimitersTest extends \PHPUnit_Framework_TestCase
+{
+    public function testUseCustomDelimiter()
+    {
+        $calculator = new StringCalculator();
+        $this->assertEquals(
+            3,
+            $calculator->add("//;\n1;2")
+        );
+    }
+
+    /**
+     * @expectedException UnexpectedValueException
+     */
+    public function testFailOnInvalidDelimiter()
+    {
+        $calculator = new StringCalculator();
+        $calculator->add("1;2");
+    }
+
+    /**
+     * @expectedException UnexpectedValueException
+     */
+    public function testFailOnDefaultDelimiterWhenUsingCustomDelimiters()
+    {
+        $calculator = new StringCalculator();
+        $calculator->add("//;\n1,2");
+    }
+}
