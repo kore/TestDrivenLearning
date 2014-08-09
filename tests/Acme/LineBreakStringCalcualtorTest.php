094aa818b32da1d1d20994d535bc201f27c7a1e9
Kore Nordmann <mail@kore-nordmann.de>
2014-08-09 14:46:19 +0200

    Added test for third step

---
+++ tests/Acme/LineBreakStringCalcualtorTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Acme;
+
+/**
+ * Allow the add() method to handle new lines between numbers (instead of
+ * commas).
+ *
+ * * the following input is ok:  "1\n2,3"  (will equal 6)
+ * * the following input is NOT ok:  "1,\n2", "1,\n"
+ * * An empty number is also not OK: "1,"
+ *
+ * @group step3
+ */
+class LineBreakStringCalcualtorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSumUpStringWithLineBreak()
+    {
+        $calculator = new StringCalculator();
+        $this->assertEquals(
+            6,
+            $calculator->add("1\n2,3")
+        );
+    }
+
+    /**
+     * @expectedException \UnexpectedValueException
+     */
+    public function testFailOnStringWithDoubleSeprator()
+    {
+        $calculator = new StringCalculator();
+        $calculator->add("1\n2,3");
+    }
+
+    /**
+     * @expectedException \UnexpectedValueException
+     */
+    public function testFailOnStringWithSeperatorAtTheEnd()
+    {
+        $calculator = new StringCalculator();
+        $calculator->add("1,");
+    }
+}
